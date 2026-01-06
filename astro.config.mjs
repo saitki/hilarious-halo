9fceede07c672ae27aecc57b957be3398bf97ccb
saitki <85718309+saitki@users.noreply.github.com>
2026-01-06 12:18:20 -0500

    features(preact): implements preact, greetings and themes dark white

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({  
-    site: "https://clever-sopapillas-e2aa52.netlify.app/"
+export default defineConfig({
+  site: "https://clever-sopapillas-e2aa52.netlify.app/",
+  integrations: [preact()]
 });
